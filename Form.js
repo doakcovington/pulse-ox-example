695bc4b7f6e9f91f9b754586750d5462e7acb0cd
doakcovington <doakcovington@protonmail.com>
2020-06-10 14:55:29 -0700

    Built out form

--- Form.js
+++ Form.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
-clas Form extends Component {
+class Form extends Component {
     initialState = {
         date: '',
-        oxygen: 0,
+        oxygen: '',
     }
 
     state = this.initialState
+
+    handleChange = event => {
+        const { date, value } = event.target
+
+        this.setState({
+            [date]: value,
+        })
+    }
+
+    render() {
+        const { date, oxygen } = this.state;
+
+        return (
+            <form>
+                <label htmlFor="date">Date</label>
+                <input
+                    type="text"
+                    name="date"
+                    id="date"
+                    value={date}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="oxygen">Oxygen</label>
+                <input 
+                    type="text"
+                    name="oxygen"
+                    id="oxygen"
+                    value={oxygen}
+                    onChange={this.handleChange}
+                />
+            </form>
+        )
+    }
 }
+
+export default Form;
