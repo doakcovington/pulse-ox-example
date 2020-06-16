0edc0523c0c4088a57ba399edfbcb5d763865762
doakcovington <doakcovington@protonmail.com>
2020-06-15 20:02:48 -0700

    Added Alert

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from 'react-bootstrap/Table'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 const TableHeader = () => {
     return (
@@ -14,12 +15,13 @@ const TableHeader = () => {
     )
 }
 
+//inline ternary operator to line 24
 const TableBody = props => {
     const rows  = props.readingData.map((row, index) => {
         return (
             <tr key={index}>
                 <td>{row.date}</td>
-                <td>{row.oxygen}</td>
+                <td>{row.oxygen}</td> 
                 <td><Button variant="danger" onClick={() => props.removeReading(index)}>Delete</Button></td>
             </tr>
         )
@@ -33,6 +35,7 @@ const InfoTable = (props) => {
             <Table bordered hover>
                 <TableHeader />
                 <TableBody readingData={ readingData } removeReading={removeReading} />
+                <Alert variant="danger">This is an error alert — check it out!</Alert>
             </Table>
         )
 }
